ced8c2414945a602d37cdae9b587e3250cdc5bca
Bohdan Bulakh <bgdn.56t@gmail.com>
2024-10-10 16:15:27 +0300

    toolbar replaced with buttons:
    - added horizontal linear layout with main menu buttons
    - added objects button onClick and onItemSelected listeners

--- app/src/main/java/com/bohdanbulakh/drawing_app/MainActivity.kt
+++ app/src/main/java/com/bohdanbulakh/drawing_app/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.bohdanbulakh.drawing_app
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.appcompat.widget.Toolbar
 import com.bohdanbulakh.drawing_app.editors.*
 
 class MainActivity : AppCompatActivity() {
-    private val submenuItems = arrayOf(R.id.line, R.id.point, R.id.rectangle, R.id.ellipse)
     private var checkedItem: MenuItem? = null
 
     private val editor = ShapeObjectsEditor
@@ -19,15 +19,16 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
+        val objectsMenuButton: Button = findViewById(R.id.objects)
+        val menu = PopupMenu(this, objectsMenuButton)
+        menu.menuInflater.inflate(R.menu.main_menu, menu.menu)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (submenuItems.contains(item.itemId)) {
+        objectsMenuButton.setOnClickListener {
+            menu.show()
+        }
+
+        menu.setOnMenuItemClickListener { item ->
             checkedItem?.isChecked = false
             checkedItem = item
             checkedItem?.isChecked = true
@@ -38,7 +39,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.rectangle -> editor.startRectEditor()
                 R.id.ellipse -> editor.startEllipseEditor()
             }
+            true
         }
-        return super.onOptionsItemSelected(item)
     }
 }
